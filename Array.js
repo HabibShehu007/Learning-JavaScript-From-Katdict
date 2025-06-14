a76ecb17af21724ed03a176a517382c4e578ce5c
HabibShehu007 <habibshehuoooo7@gmail.com>
2025-06-14 23:24:43 +0100

    Day 15 Learning Progress

---
+++ Array.js
@@ -0,0 +1,16 @@
+// Creating an Array
+let fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits); // ["Apple", "Banana", "Cherry"]
+
+// Accessing Array Elements
+let numbers = [10, 20, 30];
+console.log(numbers[1]); // 20 (index starts at 0)
+
+// Modifying Array Elements
+let colors = ["Red", "Green", "Blue"];
+colors[2] = "Yellow";
+console.log(colors); // ["Red", "Green", "Yellow"]
+
+// Checking array Length
+let animals = ["Dog", "Cat", "Bird"];
+console.log(animals.length); // 3
